77e91988c9448dafbca6993a02a684bb8b0b566e
nadaAbedAlkreem <elkahloutnada@gmail.com>
2023-04-28 23:57:53 +0300

    update

--- app/src/main/java/com/example/medicalconsultingapplication/operationConsulting/DetailsConsultingActivity.java
+++ app/src/main/java/com/example/medicalconsultingapplication/operationConsulting/DetailsConsultingActivity.java
@@ -12,11 +12,5 @@ public class DetailsConsultingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_consulting);
-
-
-
-
-
-
     }
 }
